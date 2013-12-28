fb9f81a0b0beffea0788f1c88b56b43b61eb1b08
RumeysaDelil <rumeysadelil@gmail.com>
2013-12-28 12:35:11 +0200

    update

--- src/com/example/speechap/Message.java
+++ src/com/example/speechap/Message.java
@@ -1,12 +1,22 @@
 package com.example.speechap;
 
 import android.app.Activity;
-import android.widget.Button;
+import android.os.Bundle;
 import android.widget.*;
 
 public class Message extends Activity{
-	private Button btnSpeak;
-    private EditText txtText;
+	private Button getnum;
+    private EditText writenum;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		writenum = (EditText) findViewById(R.id.editText1);
+		getnum = (Button) findViewById(R.id.button1);
+	}
+    
+    
     
 
 }
