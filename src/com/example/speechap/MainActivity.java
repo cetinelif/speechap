1a6e9c19293d0ca246a1f9e6046f0831cab73c6e
RumeysaDelil <rumeysadelil@gmail.com>
2014-01-03 16:06:35 +0200

    merge elif

--- src/com/example/speechap/MainActivity.java
+++ src/com/example/speechap/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.speechap;
 import java.util.ArrayList;
  
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,9 +20,15 @@ public class MainActivity extends Activity {
  
     protected static final int RESULT_SPEECH = 1;
  
-    //View ler tanýmlandý
     private ImageButton btnSpeak;
     private TextView txtText;
+    private TextView txtno;
+    private TextView txtmsg;
+    private Button gonder;
+    
+    private String msg;
+    
+    //yenilik
     
     
  
@@ -28,16 +37,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
  
-        txtText = (TextView) findViewById(R.id.txtText); 
+        txtText = (TextView) findViewById(R.id.txtText);
+        txtno=(TextView)findViewById(R.id.textView1);
+        
+        gonder=(Button)findViewById(R.id.button1);
+ 
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
  
-        //buton için click metodu oluþturldu
         btnSpeak.setOnClickListener(new View.OnClickListener() {
  
             @Override
             public void onClick(View v) {
  
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                Intent intent = new Intent(
+                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 //recognizerIntent
  
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
@@ -46,7 +59,7 @@ public class MainActivity extends Activity {
                     startActivityForResult(intent, RESULT_SPEECH);
                     txtText.setText("");
                     Toast t = Toast.makeText(getApplicationContext(),
-                            "Baþarýlý!",
+                            "Succes",
                             Toast.LENGTH_SHORT);
                     t.show();
                 } catch (ActivityNotFoundException a) {
@@ -57,15 +70,35 @@ public class MainActivity extends Activity {
                 }
             }
         });
+        gonder.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String no=txtno.getText().toString();
+				String sms=msg;
+				sendSMS(no, sms);
+				
+			}
+		});
  
     }
+    
+    /*
+     * 
+     * (non-Javadoc)
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
  
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
- 
+/*
+ * deneme
+ * 
+ */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -78,10 +111,22 @@ public class MainActivity extends Activity {
                         .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
  
                 txtText.setText(text.get(0));
+                msg=text.get(0).toString();
             }
+            
+          
             break;
         }
  
         }
+    }
+    
+    //sms gönderme fonksiyonu
+    private void sendSMS(String telefonNo, String mesaj)
+    {        
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+            new Intent(this, MainActivity.class), 0);                
+        android.telephony.SmsManager sms =android.telephony.SmsManager.getDefault();
+        sms.sendTextMessage(telefonNo, null, mesaj, pi, null);        
     }
 }
